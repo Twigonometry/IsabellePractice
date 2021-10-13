d2eb926cc6a7e71fc337ff456d119007537a0166
Twigonometry <mac.j.goodwin@gmail.com>
2021-10-13 22:41:10 +0100

    Attempting eval function

--- modelling/Calculator.thy
+++ modelling/Calculator.thy
@@ -29,9 +29,19 @@ datatype calc_expr = Bexp nat | Cexp calc_expr calc_op calc_expr
 
 fun calc_eval :: "calc_expr \<Rightarrow> nat" where
 "calc_eval (Bexp n) = n" |
-"calc_eval (Cexp Plus x y) = add (calc_eval x) (calc_eval y)" |
-"calc_eval (Cexp Minus x y) = sub (calc_eval x) (calc_eval y)" |
-"calc_eval (Cexp Times x y) = mul (calc_eval y) (calc_eval y)" |
-"calc_eval (Cexp Divides x y) = divide (calc_eval x) (calc_eval y)"
+"calc_eval (Cexp x Plus y) = add (calc_eval x) (calc_eval y)" |
+"calc_eval (Cexp x Minus y) = sub (calc_eval x) (calc_eval y)" |
+"calc_eval (Cexp x Times y) = mul (calc_eval y) (calc_eval y)" |
+"calc_eval (Cexp x Divides y) = divide (calc_eval x) (calc_eval y)"
+
+value "add 3 3"
+
+(* value "calc_eval Plus 3 3" *)
+(* value "calc_eval 3 Plus 3" *)
+(* value "calc_eval (Cexp 3 Plus 3)" *)
+
+value "calc_eval (Bexp 3)"
+
+value "calc_eval (Cexp (3 Plus 3))"
 
 end
