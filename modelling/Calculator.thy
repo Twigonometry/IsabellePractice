d378842fe9bc2c3f1a3d6e813dca1c9d49b214ff
Twigonometry <mac.j.goodwin@gmail.com>
2021-10-18 17:54:16 +0100

    WIP Reformatting of Calculator

--- modelling/Calculator.thy
+++ modelling/Calculator.thy
@@ -2,6 +2,9 @@ theory Calculator
 imports Main
 begin
 
+(* state is a wrapper for an int
+getInt is destructor *)
+
 datatype state = St (getInt:int)
 
 (* calculator functions on state *)
@@ -15,19 +18,28 @@ fun getResult :: "state => int" where
 (* basic arithmetic function definitions
 perform an action on a state *)
 
+(*
+fun add :: "int => state => state" where
+"add m (St n) = St (m + n)"
+
+fun sub :: "int => state => state" where
+"sub m (St n) = St (m - n)"
+
+fun mul :: "int \<Rightarrow> state => state" where
+"mul m (St n) = St (m * n)"
+
+fun divide :: "int \<Rightarrow> state => state" where
+"divide m (St n) = St (m div n)"
+*)
+
 fun add :: "state => int => state" where
 "add (St m) n = St (m + n)"
 
-fun sub :: "state => int => state" where
-"sub (St m) n = St (m - n)"
+fun clear :: "state => state" where
+"clear (St m) = St 0"
 
-fun mul :: "state \<Rightarrow> int \<Rightarrow> state" where
-"mul (St m) n = St (m * n)"
-
-fun divide :: "state \<Rightarrow> int \<Rightarrow> state" where
-"divide (St m) n = St (m div n)"
-
-value "add (St 3) 3"
+fun getResult :: "state => int" where
+"getResult (St m) = m"
 
 (* 
 model a 'session' (series of commands in python program)
@@ -36,14 +48,27 @@ calculator c = new Calculator(); (this will be boilerplate)
 c.clear().add(2).clear().getResult();
 *)
 
-datatype session = GetResult | Clear session | Add int session | Sub int session | Mul int session | Div int session
+(*
 
-term Add
+datatype session = GetResult state | Clear session | Add int session | Sub int session | Mul int session | Div int session
 
-term "Clear (Add 2 (Clear GetResult))"
+term "GetResult x"
 
-value "Add 2 GetResult"
+fun eval :: "session \<Rightarrow> state" where
+"eval (GetResult x) = x" |
+"eval (Add (int x) y) = add (int x) (eval y)"
 
-term "Clear (Add 2 (GetResult))"
+*)
+
+datatype session = GetResult state | Clear session | Add session int
+
+term GetResult
+
+fun eval :: "session \<Rightarrow> state" where
+"eval (GetResult (St x)) = x" |
+"eval (Add (session x) (int y)) = add eval(x) y" |
+"eval _ _ = undefined"
+
+term "eval y"
 
 end
