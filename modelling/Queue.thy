7eaa791327cbf579d9eea3735deececc984f21c0
Twigonometry <mac.j.goodwin@gmail.com>
2021-11-04 19:09:05 +0000

    Fix queue syntax

--- modelling/Queue.thy
+++ modelling/Queue.thy
@@ -4,18 +4,18 @@ begin
 
 (* datatype AQueue = a' list a' list *)
 
-datatype 'a queue = AQueue 'a list 'a list
+datatype 'a queue = AQueue "'a list" "'a list"
 
-definition empty :: 'a queue where
-empty = AQueue [] []
+definition empty :: "'a queue" where
+"empty = AQueue [] []"
 
-primrec enqueue :: 'a \<Rightarrow> 'a queue \<Rightarrow> 'a queue where
-enqueue x (AQueue xs ys) = AQueue (x # xs) ys
+primrec enqueue :: "'a \<Rightarrow> 'a queue \<Rightarrow> 'a queue" where
+"enqueue x (AQueue xs ys) = AQueue (x # xs) ys"
 
-fun dequeue :: 'a queue \<Rightarrow> 'a option \<times> 'a queue where
-dequeue (AQueue [] []) = (None, AQueue [] [])
-| dequeue (AQueue xs (y # ys)) = (Some y, AQueue xs ys)
-| dequeue (AQueue xs []) =
-(case rev xs of y # ys \<Rightarrow> (Some y, AQueue [] ys))
+fun dequeue :: "'a queue \<Rightarrow> 'a option \<times> 'a queue" where
+"dequeue (AQueue [] []) = (None, AQueue [] [])"
+| "dequeue (AQueue xs (y # ys)) = (Some y, AQueue xs ys)"
+| "dequeue (AQueue xs []) =
+(case rev xs of y # ys \<Rightarrow> (Some y, AQueue [] ys))"
 
 end
